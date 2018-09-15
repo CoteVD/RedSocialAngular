b9b2a225d2108718c72f88c442482f07476675d0
valeria <valeria.penaloza5@gmail.com>
2018-09-15 20:52:42 -0300

    componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+import { PrivatePageComponent } from './componentes/private-page/private-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'private', component: PrivatePageComponent},
+  {path: '**', component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
